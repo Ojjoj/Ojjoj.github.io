8ad07372a403892948a4c0ba8eeada183d234382
Karam Kassem <44134401+Ojjoj@users.noreply.github.com>
2023-12-27 23:38:33 +0200

    changes

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function getQueryParameter(name) {
 function validateDocument() {
     // Get values from the URL
     var url = getQueryParameter("url");
+    var trophy = getQueryParameter("trophy");
     var name = getQueryParameter("name");
 
     // Perform validation
-    checkValidity(url, name);
+    checkValidity(url, trophy, name);
 }
 
-function checkValidity(url, name) {
+function checkValidity(url, trophy, name) {
     // Perform asynchronous validation (you may want to use a server-side script for this)
     // For simplicity, let's assume winners.txt is a plain text file with one name per line
     fetch("winners.txt")
@@ -24,13 +25,13 @@ function checkValidity(url, name) {
             var winners = data.split("\n");
             var isValid = false;
             for (var i = 0; i < winners.length; i++){
-                if(name == winners[i].trim()){
+                if((trophy + "," + name) == winners[i].trim()){
                     isValid = true;
                     break;
                 }
             }
             // Display the result on the webpage
-            displayResult(isValid, name); 
+            displayResult(isValid, trophy, name); 
         })
         .catch(error => {
             console.error("Error reading winners.txt:", error);
@@ -38,12 +39,12 @@ function checkValidity(url, name) {
         });
 }
 
-function displayResult(isValid, name) {
+function displayResult(isValid, trophy, name) {
     var resultDiv = document.getElementById("validationResult");
     resultDiv.style.fontSize = "60px";
     resultDiv.style.textAlign = "center";
     if (isValid) {
-        resultDiv.innerHTML = "Valid Trophy : " + name ; 
+        resultDiv.innerHTML = "Valid Trophy : " + trophy + " : " + name ; 
         resultDiv.style.color = "green";
     } else {
         resultDiv.innerHTML = "Invalid Trophy Ya Nassab";
